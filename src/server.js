ef6c79fd7ca91181e786512fc9eed2f3cb47ca2b
hanna-uler <hannauler@gmail.com>
2025-07-07 15:19:30 -0400

    add getAllConts, getContById

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(getEnvVar("PORT", 3000));
 
@@ -17,15 +18,33 @@ export default function setupServer() {
             },
         }),
     );
-    app.get('/', (req, res) => {
+
+    app.get('/contacts', async (req, res) => {
+        const contacts = await getAllContacts();
         res.json({
-            message: "Hello world!",
+            status: 200,
+            message: "Successfully found contacts!",
+            data: contacts,
         });
     });
-    app.use((req, res, next) => {
+    app.get('/contacts/:contactId', async (req, res) => {
+        const contactId = req.params.contactId;
+        const contact = await getContactById(contactId);
+        if (contact === null) {
+            return res
+                .status(404)
+                .json({ message: "Contact not found" })
+        }
+        res.json({
+            status: 200,
+            message: `Successfully found contact with id ${contactId}!`,
+            data: contact,
+        });
+    });
+    app.use((req, res) => {
         res.status(404).json({
             message: 'Not found',
-        })
+        });
       });
     app.listen(PORT, (error) => {
         if (error) {
