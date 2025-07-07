3666b0d2ec1a72744cc54b286f32700dfa688132
hanna-uler <hannauler@gmail.com>
2025-07-07 11:55:09 -0400

    edit package.json - type and scripts, add index.js, env, setupServer func

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar';
+
+const PORT = Number(getEnvVar("PORT", 3000));
+
+export default function setupServer() {
+    const app = express();
+
+    app.use(express.json())
+    app.use(cors());
+    app.use(
+        pino({
+            transport: {
+                target: 'pino-pretty',
+            },
+        }),
+    );
+    app.get('/', (req, res) => {
+        res.json({
+            message: "Hello world!",
+        });
+    });
+    app.use((req, res, next) => {
+        res.status(404).json({
+            message: 'Not found',
+        })
+      });
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    });
+};
