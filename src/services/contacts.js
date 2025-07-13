f53debfd7ab3cfb3fe406437823c3ee2cc87edf7
hanna-uler <hannauler@gmail.com>
2025-07-13 14:41:54 -0400

    add pagin-on feature to getAll

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,13 +1,23 @@
 import { ContactsCollection } from "../db/models/contact.js";
+import { calculatePaginationData } from "../utils/calculatePaginationData.js";
 
-export const getAllContacts = async () => {
-    const contacts = await ContactsCollection.find();
-    return contacts;
+export const getAllContacts = async ({ page, perPage }) => {
+    const limit = perPage;
+    const skip = (page - 1) * perPage;
+    const contactsQuery = ContactsCollection.find();
+    const contactsCount = ContactsCollection.find().merge(contactsQuery).countDocuments();
+    const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+    const paginationData = calculatePaginationData(contactsCount, page, perPage);
+    return {
+        data: contacts,
+        ...paginationData,
+    };
 };
 
 export const getContactById = async (contactId) => {
     const contact = await ContactsCollection.findById(contactId);
     return contact;
+
 };
 
 export const createContact = async (payload) => {
