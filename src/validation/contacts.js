73ed7ad2ef68c950b875a60f580d39e3f73be3bd
hanna-uler <hannauler@gmail.com>
2025-07-13 09:14:05 -0400

    install Joi, add createContact valid schema

---
+++ src/validation/contacts.js
@@ -0,0 +1,23 @@
+import Joi from "joi";
+
+export const createContactSchema = Joi.object({
+    name: Joi.string().min(3).max(30).required().messages({
+        'string.base': 'Name should be a string',
+        'string.min': 'Name should have at least 3 characters',
+        'string.max': 'Name should have at most 30 characters',
+        'any.required': 'Name is required',
+      }),
+    phoneNumber: Joi.string().pattern(/^\+?[1-9]\d{1,14}$/).required().messages({
+        'string.pattern.base': 'Phone number must follow the international format',
+        'any.required': 'Phone number is required',
+      }),
+    email: Joi.string().email({minDomainSegments: 2}).required().messages({
+        'string.email': 'Email must be a valid email address',
+        'any.required': 'Email is required',
+      }),
+    isFavourite: Joi.boolean(),
+    contactType: Joi.string().valid("work", "home", "personal").required().messages({
+        'any.only': 'Contact type must be one of the following: "work", "home", "personal"',
+        'any.required': 'Contact type is required',
+    }),
+});
