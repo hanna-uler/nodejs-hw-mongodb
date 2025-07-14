e4b2d4a2d011ef96bbe2fe1163c75122a7714b30
hanna-uler <hannauler@gmail.com>
2025-07-14 15:19:53 -0400

    fix Id validation

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -3,7 +3,8 @@ import createHttpError from "http-errors";
 
 export const isValidId = (req, res, next) => {
     const { contactId } = req.params;
-    if (isValidObjectId(contactId)) {
+    if (!isValidObjectId(contactId)) {
         throw createHttpError(400, "Bad Request");
     }
+    next();
 };
