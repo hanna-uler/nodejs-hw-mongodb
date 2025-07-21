07f7bee2f21708fd97484b3c44f99d9ded5c6685
hanna-uler <hannauler@gmail.com>
2025-07-21 07:37:50 -0400

    i bcrypt, hash pass and remove it from res, add unique email check at user registr

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,14 @@
 import { UsersCollection } from "../db/models/user.js";
+import createHttpError from "http-errors";
+import bcrypt from 'bcrypt';
 
 export const registerUser = async (payload) => {
     // console.log(`at registerUser => payload: ${payload}`);
-    return await UsersCollection.create(payload);
+    const user = await UsersCollection.findOne({ email: payload.email });
+    if (user) throw createHttpError(409, "Email is in use");
+    const encryptedPass = await bcrypt.hash(payload.password, 10);
+    return await UsersCollection.create({
+        ...payload,
+        password: encryptedPass,
+    });
 };
