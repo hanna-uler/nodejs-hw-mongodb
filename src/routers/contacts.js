507f0960ed545657d4fe396c57c4972de6b09416
hanna-uler <hannauler@gmail.com>
2025-07-22 11:27:49 -0400

    add authentification to contacts route

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -4,8 +4,11 @@ import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { isValidId } from "../middlewares/isValidId.js";
+import { authenticate } from "../middlewares/authenticate.js";
 
 const router = Router();
+
+router.use(authenticate);
 
 router.get('/', ctrlWrapper(getContactsController));
 
